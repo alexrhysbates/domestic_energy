49621f9974b845b84b7540dadefbef60dc1d11e4
AlexGill <>
2024-07-21 15:54:50 +0000

    update reqs, imports

--- analyse.py
+++ analyse.py
@@ -24,6 +24,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import geopandas as gpd
+import seaborn as sns
 
 
 ############# READ / CLEAN DATASET ############################
