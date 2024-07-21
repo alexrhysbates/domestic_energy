e6f847adf41d2c96f7bd0cb28db3fdecccd696dc
AlexGill <>
2024-07-21 15:26:55 +0000

    check all runs

--- analyse.py
+++ analyse.py
@@ -22,6 +22,7 @@ logging.basicConfig(
 logging.info("Importing relevant python packages") 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import geopandas as gpd
 
 
@@ -34,10 +35,21 @@ except OSError as e:
     logging.error(f"No file found, try running compile_data.py first. \n{e}")
 
 
-# data cleaning
+logging.info("Cleaning up columns with any null values")
 df["politically_green"] = [1 if x == True else 0 for x in df.politically_green]
 
+
 ############# PLOTTING ENERGY CONSUMPTION ON A MAP ############
+logging.info("Using geopandas to load and plot energy consumption per capita on a map")
+logging.info("Read shapefile as a dataframe and tidy up columns")
+las = gpd.GeoDataFrame.from_file("data/LAD_DEC_2021_UK_BFC.shp")
+las.rename(columns={"LAD21CD":"LA"}, inplace=True)
+logging.info("Join shapefile with energy data, averaged by local authority, and plot")
+df_energy = df.groupby("LA")["energy_consumption_per_person"].mean().reset_index()
+las = las.merge(df_energy, on="LA", how="left")
+las.plot(column="energy_consumption_per_person", cmap="OrRd", edgecolor="k", legend=True)
+logging.info("Saving plot to a local .png")
+plt.savefig('energy_consumption_per_person_by_uk_local_authority.png')
 
 
 ############# EXPLORATORY ANALYSIS ############################
