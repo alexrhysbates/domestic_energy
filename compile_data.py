281e64a1b03f3c4ef5c74b3f4ce77628a5e8d211
AlexGill <>
2024-07-21 10:04:47 +0000

    start moving code into compile script

--- compile_data.py
+++ compile_data.py
@@ -25,20 +25,55 @@ Features (and associated sources / definitions / assumptions):
 
 """
 
-######### IMPORTS #########
+"""
+IMPORTS AND SET UP
+------------------
+Load relevant python packages and set up a logger
+
+"""
 import logging
 logger = logging.getLogger(__name__)
 
+logger.info("Importing relevant python packages") 
 import numpy as np
 import pandas as pd
 import geopy.distance
 
 
-#### LOADING THE DATA #####
-energy_consumption_data = pd.read_csv("data/LSOA Energy Consumption Data.csv")
+"""
+ASSUMPTIONS
+-----------
+We need to set a few assumptions on energy prices
+Taken from:
+https://www.icaew.com/insights/viewpoints-on-the-news/2022/sept-2022/chart-of-the-week-energy-price-cap-update
 
-## COMPILING THE DATASET ##
+"""
+GAS_PRICE_PER_KWH = 3.3
+ELECTRIC_PRICE_PER_KWH = 19.0
+
+
+"""
+LOADING THE DATA
+----------------
+Read in the main dataset
+"""
+logger.info("Loading main dataset on energy consumption")
+df = pd.read_csv("data/LSOA Energy Consumption Data.csv")
+logger.info(f"Data loaded, {len(df)} rows")
+
+
+"""
+COMPILING THE DATASET
+---------------------
+Do this in phases, one dataset at a time. 
+
+1. Load in new source
+2. Generate the relevant feature(s)
+3. Merge with main "df" dataset
+4. Move onto the next source!
+
+"""
 
 
 
-#
+## WRITING TO CSV
